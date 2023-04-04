4cb813503ed6e06454cf6735eb2fe11a130376f1
agostig <agostigiovanni36@gmail.com>
2023-04-04 17:45:38 +0200

    prova menu

---
+++ app/src/main/java/com/example/provabottomnav/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.provabottomnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.provabottomnav.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new FilmFragment());
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new FilmFragment());
+                    break;
+                case R.id.cinema:
+                    replaceFragment(new CinemaFragment());
+                    break;
+                case R.id.location:
+                    replaceFragment(new LocationFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
