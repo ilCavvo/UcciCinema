1ea188a5abf3e5a7de6ede9bcaecc8c86551f7d4
riccardo cavalli <riccardo.cavalli.rc@gmail.com>
2023-04-13 15:12:39 +0200

    230413 11:42

---
+++ app/src/main/java/com/example/provabottomnav/loadingPage.java
@@ -0,0 +1,41 @@
+package com.example.provabottomnav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loadingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // on below line we are configuring our window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_loading);
+
+        // on below line we are calling handler to run a task
+        // for specific time interval
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(loadingPage.this, MainActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 1500);
+
+    }
+}
